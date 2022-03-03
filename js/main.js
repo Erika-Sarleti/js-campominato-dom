c0d5bea40cdad9947987a4bc1b50d2324a1c150f
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-03 16:53:44 +0100

    problema con random

--- js/main.js
+++ js/main.js
@@ -1,26 +1,45 @@
 /*
-L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero (in ordine) tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+
+I numeri nella lista delle bombe non possono essere duplicati.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
 */
 
 const selDifficulty = document.getElementById('select-opt');
 const play =  document.getElementById('playbtn');
 let containerBox = document.querySelector('.container');
+let arrRandomNumbers =[];
 
 play.addEventListener('click', function(){
     let level = selDifficulty.value;
     if (level == 'easy') {
-        alternativeGrid(100);
+        Grid(100);
+        randomNum(100);
     }
      else if (level == 'medium') {
-    alternativeGrid(81);    
+        Grid(81); 
+        randomNum(81);
     } else {
-        alternativeGrid(49);
+        Grid(49);
+        randomNum(49);
     }
 })
-function alternativeGrid(max){
+function randomNum(max){
+    while (arrRandomNumbers.lenght <= 16){
+        const randomNumber = Math.floor(Math.random() * (max - 1) + 1);
+        if (!arrRandomNumbers.includes(randomNumber)){
+            arrRandomNumbers.push(randomNumber);
+        }
+    }
+    console.log(arrRandomNumbers);
+}
+
+
+
+function Grid(max){
     let lato = Math.sqrt(max);
     let square = document.querySelector('.square');
     for (i = 1; i <= max; i++){
