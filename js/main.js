3e5e5605cecef93ab635c49167774dd6e75ccf37
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-03 20:25:09 +0100

    cambio colori celle

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 /*
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-
-I numeri nella lista delle bombe non possono essere duplicati.
-
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
@@ -11,9 +7,10 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 const selDifficulty = document.getElementById('select-opt');
 const play =  document.getElementById('playbtn');
 let containerBox = document.querySelector('.container');
-let arrRandomNumbers =[];
+let arrRandomNumbers;
 
 play.addEventListener('click', function(){
+    containerBox.innerHTML = '';
     let level = selDifficulty.value;
     if (level == 'easy') {
         Grid(100);
@@ -26,18 +23,18 @@ play.addEventListener('click', function(){
         Grid(49);
         randomNum(49);
     }
+    console.log(arrRandomNumbers);
+    containerBox.classList.remove('no-pointer');
 })
 function randomNum(max){
+    arrRandomNumbers =[];
     while (arrRandomNumbers.length < 16){
-        const randomNumber = Math.floor(Math.random() * (max - 1) + 1);
+        let randomNumber = Math.floor(Math.random() * (max - 1) + 1);
         if (!(arrRandomNumbers.includes(randomNumber))){
             arrRandomNumbers.push(randomNumber);
         }
     }
-    // console.log(arrRandomNumbers);
-    return(arrRandomNumbers);
-}
-
+}   
 
 
 
@@ -45,17 +42,27 @@ function Grid(max){
     let lato = Math.sqrt(max);
     let square = document.querySelector('.square');
     for (i = 1; i <= max; i++){
-       let square = document.createElement('div');
-       square.classList.add('square');
-       square.innerHTML= i; 
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML= i; 
        square.style.width =`calc(100% / ${lato})`;
        square.style.height = `calc(100% / ${lato})`;
+       square.addEventListener('click', changeColor);
        containerBox.append(square);
-       square.addEventListener('click', changeColor)
+       
+       function changeColor(){
+           console.log(square.innerHTML);
+           for (j = 0; j < 16; j++){
+               if (square.innerHTML == arrRandomNumbers[j]){
+                   this.classList.add('bomb');
+                   containerBox.classList.add('no-pointer');
+                   alert(`Hai perso! Punteggio: ${document.querySelectorAll('.selected').length}`);
+                   
+               }else{
+                   this.classList.add('selected');
+               } 
+           }
+       }
     }    
     // play.remove();
-}
- 
-function changeColor(){
-    this.classList.add('selected');
 }
